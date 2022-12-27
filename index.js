1e0341fae88c15cfe818ab5e0805bc17fde92a35
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2022-12-27 21:32:04 +0600

    database connect

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app =express ();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =process.env.PORT|| 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+console.log(process.env.DB_User)
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ij3fmcd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('its working')
